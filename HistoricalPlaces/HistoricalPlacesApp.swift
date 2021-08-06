ff7eac3f28bd9e9465f617bddff0364ce1935718
Abdülsamet Ayyıldız <85462491+AbdulsametAyyildiz@users.noreply.github.com>
2021-08-07 00:23:41 +0300

    Initial Commit

---
+++ HistoricalPlaces/HistoricalPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoricalPlacesApp.swift
+//  HistoricalPlaces
+//
+//  Created by Abdussamed on 7.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct HistoricalPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
